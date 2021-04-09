9adfdb2963eeb8941426b633463a799e0fac8e8e
Николай Медведев <pengu1n4ik.pi@yandex.ru>
2021-04-09 17:11:16 +0300

    feat: create main content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
   speed: 700,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
 
   // Navigation arrows
   navigation: {
@@ -15,9 +32,16 @@ const swiper = new Swiper('.channel-slider', {
 const swiperRecomended = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
   speed: 700,
-
+  spaceBetween: 20,
+  breakpoints: {
+    1300: {
+      slidesPerView: 3
+    },
+    1000: {
+      slidesPerView: 2
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.recommended-button-next',
@@ -29,9 +53,25 @@ const swiperRecomended = new Swiper('.recommended-slider', {
 const swiperSubscription = new Swiper('.subscription-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
   speed: 700,
-
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.subscription-button-next',
@@ -39,3 +79,17 @@ const swiperSubscription = new Swiper('.subscription-slider', {
   },
 
 });
+
+const searchBtn = document.querySelector('.mobile-search'),
+      mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+});
+
+if(document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  swiperRecomended.destroy();
+  swiperSubscription.destroy();
+}
+
